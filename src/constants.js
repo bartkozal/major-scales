427c07af3521760dc2368bbd8f3c795e5870f80b
Bart Kozal <bkzl@me.com>
2018-05-14 16:58:30 +0200

    Provide correct scales

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,16 @@
 export const QUIZ_LENGTH = 10;
 export const MAJOR_SCALES = [
   { name: "C Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "G Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "D Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "A Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "E Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "B Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "F# Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "Gb Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "Db Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "Ab Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "Eb Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "Bb Major", notes: ["C", "D", "E", "F", "G", "A", "B"] },
-  { name: "F Major", notes: ["C", "D", "E", "F", "G", "A", "B"] }
+  { name: "G Major", notes: ["G", "A", "B", "C", "D", "E", "F#"] },
+  { name: "D Major", notes: ["D", "E", "F#", "G", "A", "B", "C#"] },
+  { name: "A Major", notes: ["A", "B", "C#", "D", "E", "F#", "G#"] },
+  { name: "E Major", notes: ["E", "F#", "G#", "A", "B", "C#", "D#"] },
+  { name: "B Major", notes: ["B", "C#", "D#", "E", "F#", "G#", "A"] },
+  { name: "F# Major", notes: ["F#", "G#", "A#", "B", "C#", "D#", "E#"] },
+  { name: "Gb Major", notes: ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"] },
+  { name: "Db Major", notes: ["Db", "Eb", "F", "G", "Ab", "Bb", "Cb"] },
+  { name: "Ab Major", notes: ["Ab", "Bb", "Cb", "D", "Eb", "F", "G"] },
+  { name: "Eb Major", notes: ["Eb", "F", "G", "Ab", "Bb", "C", "D"] },
+  { name: "Bb Major", notes: ["Bb", "C", "D", "Eb", "F", "G", "A"] },
+  { name: "F Major", notes: ["F", "G", "A", "Bb", "C", "D", "E"] }
 ];
