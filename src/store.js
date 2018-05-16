4210f3cf69710cd3fc6c5222cd15cf5bbfc2db03
Bart Kozal <bkzl@me.com>
2018-05-16 17:55:48 +0200

    Limit redux-logger to development

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./state";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const middlewares = process.env.NODE_ENV === "development" ? [logger] : [];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
