c7e21e7838b2ee38fb1108adc6473059523fdf37
Bart Kozal <bkzl@me.com>
2018-05-16 19:18:51 +0200

    Update answer output

--- src/Answer/Answer.js
+++ src/Answer/Answer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 
-const Answer = ({ isAnswerCorrect, noteNumber, scaleName, correctAnswer }) => (
+const Answer = ({ isAnswerCorrect, correctAnswer }) => (
   <div
     className={classNames("Answer", [
       isAnswerCorrect ? "Answer--correct" : "Answer--incorrect"
@@ -11,8 +11,7 @@ const Answer = ({ isAnswerCorrect, noteNumber, scaleName, correctAnswer }) => (
       "Correct, nice!"
     ) : (
       <div>
-        Incorrect. The {noteNumber} note in {scaleName} is:{" "}
-        <strong>{correctAnswer}</strong>
+        Incorrect. The answer is: <strong>{correctAnswer}</strong>
       </div>
     )}
   </div>
--- src/Answer/index.js
+++ src/Answer/index.js
@@ -2,30 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import Answer from "./Answer";
 
-const AnswerContainer = ({
-  isAnswerCorrect,
-  noteNumber,
-  scaleName,
-  correctAnswer,
-  currentQuestion
-}) => (
-  <Answer
-    isAnswerCorrect={isAnswerCorrect}
-    noteNumber={noteNumber}
-    scaleName={scaleName}
-    correctAnswer={correctAnswer}
-  />
+const AnswerContainer = ({ isAnswerCorrect, correctAnswer }) => (
+  <Answer isAnswerCorrect={isAnswerCorrect} correctAnswer={correctAnswer} />
 );
 
-const mapStateToProps = ({ questionsSet, currentQuestion }) => {
-  const question = questionsSet[currentQuestion];
-
-  return {
-    noteNumber: question.noteNumber,
-    scaleName: question.scaleName,
-    correctAnswer: question.correctAnswer,
-    currentQuestion
-  };
-};
+const mapStateToProps = ({ questionsSet, currentQuestion }) => ({
+  correctAnswer: questionsSet[currentQuestion].correctAnswer
+});
 
 export default connect(mapStateToProps)(AnswerContainer);
